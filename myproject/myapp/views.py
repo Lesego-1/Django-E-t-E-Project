0c974ef636a21dc9904a1f4445fefb7d4c6b0e23
Lesego <lesegojuniorrobert7@gmail.com>
2024-12-14 09:53:40 +0200

    update views.py

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from .forms import ClassificationForm
+import joblib
+import pandas as pd
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        form = ClassificationForm(request.POST)
+        if form.is_valid():
+            # Extract data and format it for the model
+            input_data = pd.DataFrame([form.cleaned_data])
+            
+            # Load the model
+            model = joblib.load(r"myproject\myapp\ml_models\rf_model.pkl")
+            
+            prediction = model.predict(input_data)[0]
+            result = "Yes" if prediction == 1 else "No"
+            
+            return render(request, "result.html", {"result": result})
+    else:
+        form = ClassificationForm()
+    return render(request, 'index.html', {"form":form})
