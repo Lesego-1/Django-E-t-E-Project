a54998c981f84ebb26f72dcc89b3cbbb83bc89e0
Lesego <lesegojuniorrobert7@gmail.com>
2024-12-14 10:43:11 +0200

    remove result view

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -9,10 +9,10 @@ def index(request):
         form = ClassificationForm(request.POST)
         if form.is_valid():
             # Extract data and format it for the model
-            input_data = pd.DataFrame([form.cleaned_data])
+            input_data = pd.DataFrame([form.cleaned_data.values()])
             
             # Load the model
-            model = joblib.load(r"myproject\myapp\ml_models\rf_model.pkl")
+            model = joblib.load(r"C:\Programming\Python\Industry Level Projects\Django E-t-E Project\myproject\myapp\ml_models\rf_model.pkl")
             
             prediction = model.predict(input_data)[0]
             result = "Yes" if prediction == 1 else "No"
@@ -21,3 +21,6 @@ def index(request):
     else:
         form = ClassificationForm()
     return render(request, 'index.html', {"form":form})
+
+# def result(request):
+#     return render(request, 'result.html')
