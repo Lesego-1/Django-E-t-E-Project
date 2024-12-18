922bf2dbe603473514b07cdfc594e0ee9c78efe7
Lesego <lesegojuniorrobert7@gmail.com>
2024-12-18 16:21:30 +0200

    Add scaler to views.py

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -7,12 +7,6 @@ class ClassificationForm(forms.Form):
         min_value=0,
         widget=forms.TextInput(attrs={"placeholder": "Enter the last contact duration in seconds:"})
     )
-    annual_balance = forms.IntegerField(
-        label="Annual Balance",
-        required=True,
-        min_value=0,
-        widget=forms.TextInput(attrs={"placeholder": "Enter your annual balance:"})
-    )
     age = forms.IntegerField(
         label="Your Age",
         required=True,
@@ -20,18 +14,24 @@ class ClassificationForm(forms.Form):
         max_value=120,
         widget=forms.TextInput(attrs={"placeholder": "Enter your age:"})
     )
-    contact_day = forms.IntegerField(
-        label="Last contact day",
-        required=True,
-        widget=forms.TextInput(attrs={"placeholder":"Enter the last contact day of the month:"})
-    )
     outcome_choices = [
         (1, 'Yes'),
         (0, 'No')
     ]
-    poutcome = forms.ChoiceField(
-        label="Previous outcome",
+    housing_loan = forms.ChoiceField(
+        label="Do you have a housing loan?",
         required=True,
         choices=outcome_choices,
         widget=forms.RadioSelect
+    )
+    annual_balance = forms.IntegerField(
+        label="Annual Balance",
+        required=True,
+        min_value=0,
+        widget=forms.TextInput(attrs={"placeholder": "Enter your annual balance:"})
+    )
+    contact_day = forms.IntegerField(
+        label="Last contact day",
+        required=True,
+        widget=forms.TextInput(attrs={"placeholder":"Enter the last contact day of the month:"})
     )
