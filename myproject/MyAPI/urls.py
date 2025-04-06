24717b33dd77ad13d63b22a24c91c9a25b7eecff
Lesego <lesegojuniorrobert7@gmail.com>
2025-04-06 13:26:27 +0200

    Removing extra files

--- myproject/MyAPI/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("api/", views.CustomersListCreate.as_view(), name="api"),
-    path("api/<int:pk>/", views.CustomersRetrieveUpdateDestroy.as_view(), name="update"),
-    path("", views.formProcessing, name='form'),
-]
